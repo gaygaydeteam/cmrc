132d889bf078f14d38ff1882273061fa6dd61979
john <john@192.168.2.107>
2018-09-04 22:31:14 +0800

    exchange page

--- src/pages/Exchange.vue
+++ src/pages/Exchange.vue
@@ -1,12 +1,88 @@
 <template>
-    <div>
-        <h1>This is exchange page!</h1>
+    <div class="exchange-page">
+        <div class="header-img">
+            <img :src="headerImg">
+        </div>
+        <div class="exchange-contain">
+            <div class="btn-wrapper">
+                <span class="hang-up">挂卖</span>
+                <span class="buy">求购</span>
+            </div>
+            <table class="exchange-table">
+                <thead>
+                    <tr>
+                        <th class="col">账号</th>
+                        <th class="col">数量</th>
+                        <th class="col">金额</th>
+                        <th class="col">日期</th>
+                        <th class="col">操作</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="item in itemList" :key="item.id">
+                        <td>{{item.account}}</td>
+                        <td>{{item.number}}</td>
+                        <td>{{item.money}}</td>
+                        <td>{{item.date}}</td>
+                        <td><button class="send">出售</button></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Exchange'
+    name: 'Exchange',
+    data () {
+        return {
+            headerImg: '',
+            itemList: [{
+                id: 1,
+                account: '1234567',
+                number: '300',
+                money: '255.00',
+                date: '2018.08.28'
+            }, {
+                id: 2,
+                account: '1234567',
+                number: '300',
+                money: '255.00',
+                date: '2018.08.28'
+            }, {
+                id: 3,
+                account: '1234567',
+                number: '300',
+                money: '255.00',
+                date: '2018.08.28'
+            }, {
+                id: 4,
+                account: '1234567',
+                number: '300',
+                money: '255.00',
+                date: '2018.08.28'
+            }, {
+                id: 5,
+                account: '1234567',
+                number: '300',
+                money: '255.00',
+                date: '2018.08.28'
+            }, {
+                id: 6,
+                account: '1234567',
+                number: '300',
+                money: '255.00',
+                date: '2018.08.28'
+            }, {
+                id: 7,
+                account: '1234567',
+                number: '300',
+                money: '255.00',
+                date: '2018.08.28'
+            }]
+        }
+    }
 }
 </script>
 
